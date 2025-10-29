f23c0aa68c7c27f4269ae80117039c7805a0ec81
Yehonatan Balzer <yehonatan.balzer@gmail.com>
2025-10-29 13:44:38 +0200

    Delete manoa/Backened directory

--- manoa/Backened/filters/TextFilter/Lower.py
+++
@@ -1,9 +0,0 @@
-from Backened.filters.TextFilter.TextFilter import *
-
-
-class Lower(TextFilter):
-    def __init__(self):
-        super().__init__("lower")
-
-    def filter(self, text_to_filter: str):
-        return text_to_filter.lower()
